c4e41de4cc7cc23490fe2aad97434b8afaeb7799
ArneTVdobbel <arnetdirekte@gmail.com>
2025-02-11 15:28:49 +0100

    Oppdatert mappestruktur for artikler og fikset URL-stier

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-app.use(express.static('public'));
+// Server statiske filer fra public-mappen
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Håndter rotendepunktet
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Håndter /kalender endepunktet
+app.get('/kalender', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'kalender', 'index.html'));
+});
+
+// Start serveren
 app.listen(port, () => {
     console.log(`Server kjører på http://localhost:${port}`);
 }); 
